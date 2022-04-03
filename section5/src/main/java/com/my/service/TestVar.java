3d9cc4c3473d06b6acdef931d7f4b2b49f280bd4
cs Wang <csw.dawn@gmail.com>
2022-04-03 16:53:13 +0800

    feat: Update section5

---
+++ section5/src/main/java/com/my/service/TestVar.java
@@ -0,0 +1,9 @@
+package section5.src.main.java.com.my.service;
+
+public class TestVar {
+    public static void main(String[] args) {
+        // x 是變數名稱, 0 是賦予該變數的數值
+        int x = 0;
+        System.out.println("x = " + x);
+    }    
+}
